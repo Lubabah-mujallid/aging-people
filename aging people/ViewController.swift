1e80f7805670ef9bdabae1a373a771323718a743
Lubabah-mujallid <90623112+Lubabah-mujallid@users.noreply.github.com>
2021-12-08 19:43:51 +0000

    ++

--- aging people/ViewController.swift
+++ aging people/ViewController.swift
@@ -1,19 +1,30 @@
-//
-//  ViewController.swift
-//  aging people
-//
-//  Created by admin on 08/12/2021.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    let peopl = ["ahmed", "mohammad", "khalid", "ibrahim", "lubabah", "lolo", "lily", "petty", "nine", "peter", "quin", "penny"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
+}
 
-
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return peopl.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let random = Int.random(in: 1...90)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = peopl[indexPath.row]
+        cell.detailTextLabel?.text = "\(random) years old"
+        return cell
+    }
+    
+    
 }
 
